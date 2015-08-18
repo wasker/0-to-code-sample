94297f960c06df994c1c26ed43d74265c13e7339
wasker <wasker@users.noreply.github.com>
2015-08-17 21:55:35 -0700

    Remove rimraf from gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@
 /* global __dirname */
 
 var gulp = require("gulp"),
-  rimraf = require("rimraf"),
   concat = require("gulp-concat"),
   cssmin = require("gulp-cssmin"),
   uglify = require("gulp-uglify"),
